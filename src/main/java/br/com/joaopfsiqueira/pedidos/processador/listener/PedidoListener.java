4023e0345828e269d8223161c7ea26f24a7e7817
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-05 13:42:13 -0300

    fix: ajustando o PedidoListener para mandar corretamente ao pedidoservice.

--- src/main/java/br/com/joaopfsiqueira/pedidos/processador/listener/PedidoListener.java
+++ src/main/java/br/com/joaopfsiqueira/pedidos/processador/listener/PedidoListener.java
@@ -2,6 +2,7 @@ package br.com.joaopfsiqueira.pedidos.processador.listener;
 
 import br.com.joaopfsiqueira.pedidos.processador.entity.Pedido;
 import br.com.joaopfsiqueira.pedidos.processador.entity.enums.Status;
+import br.com.joaopfsiqueira.pedidos.processador.service.PedidoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -10,12 +11,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class PedidoListener {
 
-    private final Logger logger = LoggerFactory.getLogger(PedidoListener.class);
+    private final PedidoService pedidoService;
+
+    public PedidoListener(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
 
     // metodo que será chamado quando uma mensagem for recebida
     @RabbitListener(queues = "pedidos.v1.processamento")
     public void enviarNotificacao(Pedido pedido){
         pedido.setStatus(Status.PROCESSADO);
-        logger.info("Notificacao gerada: {}", pedido.toString());
+        pedidoService.save(pedido);
     }
 }
