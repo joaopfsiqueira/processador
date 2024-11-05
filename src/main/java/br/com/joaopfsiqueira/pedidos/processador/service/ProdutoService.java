16bcc4042ef33b3453bf70bff1d0a4ccab6b0b40
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-05 13:29:40 -0300

    feat: produto service

--- src/main/java/br/com/joaopfsiqueira/pedidos/processador/service/ProdutoService.java
+++ src/main/java/br/com/joaopfsiqueira/pedidos/processador/service/ProdutoService.java
@@ -1,7 +1,10 @@
 package br.com.joaopfsiqueira.pedidos.processador.service;
 
+import br.com.joaopfsiqueira.pedidos.processador.entity.ItemPedido;
 import br.com.joaopfsiqueira.pedidos.processador.repository.ProdutoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProdutoService {
@@ -10,5 +13,11 @@ public class ProdutoService {
 
     public ProdutoService(ProdutoRepository produtoRepository) {
         this.produtoRepository = produtoRepository;
+    }
+
+    public void save(List<ItemPedido> itens) {
+        itens.forEach(item -> {
+            produtoRepository.save(item.getProduto()); // salva o produto no banco, o produto está na entity
+        });
     }
 }
