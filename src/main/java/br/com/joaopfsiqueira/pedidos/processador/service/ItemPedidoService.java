4942f717a92b54304e7564fc864b09bc7100573c
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-05 13:37:29 -0300

    feat: novos métodos

--- src/main/java/br/com/joaopfsiqueira/pedidos/processador/service/ItemPedidoService.java
+++ src/main/java/br/com/joaopfsiqueira/pedidos/processador/service/ItemPedidoService.java
@@ -1,6 +1,7 @@
 package br.com.joaopfsiqueira.pedidos.processador.service;
 
 import br.com.joaopfsiqueira.pedidos.processador.entity.ItemPedido;
+import br.com.joaopfsiqueira.pedidos.processador.entity.Pedido;
 import br.com.joaopfsiqueira.pedidos.processador.repository.ItemRepository;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +18,17 @@ public class ItemPedidoService {
 
     public List<ItemPedido> save(List<ItemPedido> itens) {
         return itemRepository.saveAll(itens);
+    }
+
+    // diferente do metodo acima ele não salva um vetor inteiro, ele salva apenas um elemento!
+    public void save(ItemPedido itemPedido) {
+        itemRepository.save(itemPedido);
+    }
+
+    public void updatedItemPedido(List<ItemPedido> itemPedidos, Pedido pedido) {
+        itemPedidos.forEach(item -> {
+            item.setPedido(pedido); // informando ao item o seu pedido!
+            itemRepository.save(item);
+        });
     }
 }
