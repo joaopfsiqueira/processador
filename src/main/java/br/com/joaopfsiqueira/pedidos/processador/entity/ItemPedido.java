f9472db0215e7709fd9c7161f79ace247b41ee26
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-05 10:37:45 -0300

    feat: JPA item pedido

--- src/main/java/br/com/joaopfsiqueira/pedidos/processador/entity/ItemPedido.java
+++ src/main/java/br/com/joaopfsiqueira/pedidos/processador/entity/ItemPedido.java
@@ -1,5 +1,9 @@
 package br.com.joaopfsiqueira.pedidos.processador.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +14,18 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "item_pedido")
 public class ItemPedido {
 
+    @Id
     private UUID id = UUID.randomUUID();
+
+    @ManyToOne
     private Produto produto;
+
     private Integer quantidade;
+
+    @ManyToOne
+    private Pedido pedido;
 }
