bc01342b519fdf373def553cee18d3b638a77a25
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-05 13:31:17 -0300

    feat: itempedido service

--- src/main/java/br/com/joaopfsiqueira/pedidos/processador/service/ItemPedidoService.java
+++ src/main/java/br/com/joaopfsiqueira/pedidos/processador/service/ItemPedidoService.java
@@ -1,7 +1,10 @@
 package br.com.joaopfsiqueira.pedidos.processador.service;
 
+import br.com.joaopfsiqueira.pedidos.processador.entity.ItemPedido;
 import br.com.joaopfsiqueira.pedidos.processador.repository.ItemRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ItemPedidoService {
@@ -10,5 +13,9 @@ public class ItemPedidoService {
 
     public ItemPedidoService(ItemRepository itemRepository) {
         this.itemRepository = itemRepository;
+    }
+
+    public List<ItemPedido> save(List<ItemPedido> itens) {
+        return itemRepository.saveAll(itens);
     }
 }
