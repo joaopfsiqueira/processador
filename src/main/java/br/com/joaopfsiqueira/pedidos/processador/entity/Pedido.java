bca38080e3629e89bf65f28c7327164a806abf0a
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2024-11-05 10:37:50 -0300

    feat: JPA pedido

--- src/main/java/br/com/joaopfsiqueira/pedidos/processador/entity/Pedido.java
+++ src/main/java/br/com/joaopfsiqueira/pedidos/processador/entity/Pedido.java
@@ -3,6 +3,7 @@ package br.com.joaopfsiqueira.pedidos.processador.entity;
 import br.com.joaopfsiqueira.pedidos.processador.entity.ItemPedido;
 import br.com.joaopfsiqueira.pedidos.processador.entity.enums.Status;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,14 +16,26 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity //
+@Table(name = "pedido")
 public class Pedido {
 
+    @Id
     private UUID id = UUID.randomUUID();
     private String cliente;
+
+    @OneToMany(mappedBy = "pedido") // um pedido tem muitos itens
     private List<ItemPedido> itens  = new ArrayList<>();
+
+    @Column(name = "valor_total")
     private Double valorTotal;
+
+    @Column(name = "email_notificacao")
     private String emailNotificacao;
+
+    @Enumerated(EnumType.STRING) // salva o nome do enum no banco de dados
     private Status status = Status.EM_PROCESSAMENTO;
+
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     private LocalDateTime dataHora = LocalDateTime.now(); // na criação do pedido, a data e hora atual é atribuída
 
